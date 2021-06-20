0d062bb8597602d9b45dcad092928d5d78fa59f7
Sanjay Singh Patel <57282318+sanjay-singh-patel@users.noreply.github.com>
2021-06-20 11:51:45 +0530

    Add files via upload

---
+++ Dice.py
@@ -0,0 +1,22 @@
+import random
+
+
+# Class for a dice
+
+class Dice:
+
+    def __init__(self):
+        self.value = random.randint(1, 6)
+
+    def roll(self) -> int:
+        self.value = random.randint(1, 6)
+        return self.value
+
+
+my_dice = Dice()
+while 1:
+    print("press 1 to roll the dice\npress 0 to quit")
+    if int(input()) == 1:
+        print(int(my_dice.roll()))
+    else:
+        break
